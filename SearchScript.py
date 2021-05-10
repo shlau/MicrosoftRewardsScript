7a84815126810b58bd1551415085b14affb9e75f
Sheldon <sheldonlau1@gmail.com>
2021-05-09 18:05:44 -0700

    moved configurable variables to the top

--- SearchScript.py
+++ SearchScript.py
@@ -5,12 +5,15 @@ import json
 import random
 from selenium import webdriver
 from selenium.common.exceptions import ElementNotInteractableException
-
+DRIVER_PATH = '$YOUR CHROMEDRIVER PATH e.g. /Users/myname/Documents/MSFTRewards/chromedriver'
+USERNAME = '$YOUR USERNAME'
+PASSWORD = '$YOUR PASSWORD'
+NUM_MOBILE_SEARCHES = 35
+NUM_PC_SEARCHES = 50
 def startWebDriver(user_agent):
     opts = webdriver.ChromeOptions()
     opts.add_argument("user-agent={}".format(user_agent))
-    driver_path = '$CHROME_DRIVER_PATH'
-    driver = webdriver.Chrome(executable_path=driver_path,options=opts)
+    driver = webdriver.Chrome(executable_path=DRIVER_PATH,options=opts)
     return driver
 
 def confirmSignIn(driver, isMobile):
@@ -35,12 +38,12 @@ def loginToRewards(driver, isMobile):
     driver.get(login_url)
     user_id_element = driver.find_element_by_css_selector('input[type="email"]')
     submit_button_element = driver.find_element_by_css_selector('input[type="submit"]')
-    user_id_element.send_keys('$USERNAME')
+    user_id_element.send_keys(USERNAME)
     submit_button_element.click()
 
     time.sleep(5)
     passwd_element = driver.find_element_by_css_selector('input[type="password"]')
-    passwd_element.send_keys('$PASSWORD')
+    passwd_element.send_keys(PASSWORD)
     time.sleep(5)
     login_button = driver.find_element_by_css_selector('input[type="submit"]')
     actions = webdriver.ActionChains(driver)
@@ -81,8 +84,6 @@ def completeDailySearches(user_agent,num_searches,base_url,isMobile):
 
 base_url = 'https://www.bing.com/search?q='
 edge_user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36 Edg/89.0.774.63'
-num_pc_searches = 50
 android_user_agent = "Mozilla/5.0 (Android 6.0.1; Mobile; rv:63.0) Gecko/63.0 Firefox/63.0"
-num_mobile_searches = 35
-completeDailySearches(edge_user_agent,num_pc_searches,base_url,False);
-completeDailySearches(android_user_agent,num_mobile_searches,base_url,True);
+completeDailySearches(edge_user_agent,NUM_PC_SEARCHES,base_url,False);
+completeDailySearches(android_user_agent,NUM_MOBILE_SEARCHES,base_url,True);
